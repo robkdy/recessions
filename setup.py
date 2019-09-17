9f91fee0937c50d3c54e32a79a67025dbafbbe0b
robkdy <rob2tran@gmail.com>
2019-09-17 13:56:37 -0500

    Initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict recessions using FRED economic data',
+    author='Robin Tran',
+    license='',
+)
